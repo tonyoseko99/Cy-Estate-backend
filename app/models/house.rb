c1d33b81cfbeb24d8badd8c31a37ac83d93c1a41
Tonny Oseko <tonnyoseko0@gmail.com>
2022-12-24 14:28:12 +0300

    feat: add logic to the houses controller

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,2 +1,12 @@
 class House < ApplicationRecord
+    def index
+        @houses = House.all
+        render json: @houses
+    end
+
+    def show
+        @house = House.find(params[:id])
+        render json: @house
+    end
+
 end

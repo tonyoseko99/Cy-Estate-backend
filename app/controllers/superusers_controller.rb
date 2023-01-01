01f55da4ea987cb53bc98f3d1e5bff8633e17d55
Tonny Oseko <tonnyoseko0@gmail.com>
2023-01-01 13:43:34 +0300

    create logic for the superusers controller

--- app/controllers/superusers_controller.rb
+++ app/controllers/superusers_controller.rb
@@ -1,2 +1,26 @@
 class SuperusersController < ApplicationController
+    def new
+      @superuser = Superuser.new
+    end
+  
+    def create
+      @superuser = Superuser.new(superuser_params)
+  
+      if @superuser.save
+        redirect_to @superuser, notice: 'Superuser was successfully created.'
+      else
+        render :new
+      end
+    end
+  
+    def destroy
+      @superuser = Superuser.find(params[:id])
+      @superuser.destroy
+      redirect_to superusers_url, notice: 'Superuser was successfully destroyed.'
+    end
+  
+    private
+      def superuser_params
+        params.require(:superuser).permit(:email, :password, :password_confirmation, :role)
+      end
 end
--- app/models/superuser.rb
+++ app/models/superuser.rb
@@ -1,2 +1,7 @@
 class Superuser < ApplicationRecord
+    # Add any additional attributes that you want to include for the superuser, such as role or permissions
+    attr_accessor :role
+
+    # Enable password authentication for the superuser
+    has_secure_password
 end
